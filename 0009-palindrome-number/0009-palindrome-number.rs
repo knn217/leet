dae08c0e7b782fdecb5a07d767fec21bc22b45e8
knn217 <86541274+knn217@users.noreply.github.com>
2026-09-18 19:19:17 +0700

    Time: 444 ms (6.1%), Space: 2.1 MB (54.04%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.rs
+++ 0009-palindrome-number/0009-palindrome-number.rs
@@ -1,24 +1,34 @@
 impl Solution {
+    fn digit(val: i32, idx: u32) -> i32{
+        const base: i32 = 10;
+        let mut digit = val;
+        // Remove lower digits by division flooring
+        for i in 0..(idx) {
+            digit /= base;
+        }
+        println!("digit after removed lower {}", digit);
+        // remove upper digits: get remainder
+        digit %= base;
+        println!("digit after removed upper {}", digit);
+        println!("idx {} has digit {}", idx, digit);
+        digit
+    }
     pub fn is_palindrome(x: i32) -> bool {
         // Negative numbers are never Palindrome
         if x < 0 { return false; }
         // Handle normal numbers
-        let mut x = x;
-        let mut digits: Vec::<i32> = Vec::new();
-        loop {
-            let mut remainder;
-            (x, remainder) = (x / 10, x % 10);
-            // println!("{}", remainder);
-            // Push the remainder
-            digits.push(remainder);
-            if x == 0 { break; }
+        let mut range = x.clone();
+        let mut max_dec = 0;
+        const base: i32 = 10;
+        while (0 < range) {
+            range /= 10;
+            max_dec += 1;
         }
-        for idx in 0..digits.len() {
-            let idx_rev = digits.len() - 1 - idx;
-            // If reached/exceeded the middle digit, break loop
-            if idx >= idx_rev { break; }
-            let val = digits[idx];
-            let val_rev = digits[idx_rev];
+        println!("{}", max_dec);
+        for dec in 0..(max_dec/2) {
+            let val_rev = Self::digit(x, max_dec - dec - 1);
+            let val = Self::digit(x, dec);
+            println!("{}, {}", val, val_rev);
             // Return false if 1 pair violates the palindrome rule
             if val != val_rev { return false; }
         }
