1a617eccc93e871c3be7a92b44a03e325ca0bc5c
knn217 <86541274+knn217@users.noreply.github.com>
2026-09-19 01:34:26 +0700

    Time: 0 ms (100%), Space: 2.6 MB (30.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.rs
@@ -1,15 +1,17 @@
 
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
-        let mut map = vec![usize::MAX; 128]; // Vec of 128 element, init as 0
+        let mut map = vec![usize::MAX; 128]; // Vec of 128 element, use MAX as init value
         let mut start = 0;
         let mut max = 0;
         for (end, ch) in s.chars().enumerate() {
             let b = ch as usize;
             if usize::MAX != map[b] {
-                start = start.max(map[b]);
+                // Found valid index for current char -> a repeat
+                start = start.max(map[b]); // start takes the max of the 2 repeats
             }
             max = max.max(end + 1 - start);
+            // Update the index in map
             map[b] = end + 1;
         }
         max as i32
