b25370de4bf3d8405da842a92b10ee11f9b3b87b
knn217 <86541274+knn217@users.noreply.github.com>
2026-09-20 14:56:46 +0700

    Time: 0 ms (100%), Space: 2.2 MB (57.34%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.rs
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.rs
@@ -36,29 +36,29 @@ impl Solution {
         let mut current = list_start.as_mut();
         while let Some(node) = current.take() {
             if list_other == None {
-                println!("Other is none, all nodes are in main list");
+                // println!("Other is none, all nodes are in main list");
                 break;
             }
             if node.next == None {
-                println!("Main's next is none, all remaining nodes are in other list, moving other into main's next");
+                // println!("Main's next is none, all remaining nodes are in other list, moving other into main's next");
                 node.next = list_other.take();
                 break;
             }
             let next_val = node.next.as_ref().unwrap().val;
             let other_val = list_other.as_ref().unwrap().val;
             if next_val >= other_val {
-                println!("other's current value {} <= next value {}, swapping other and next", other_val, next_val);
-                println!("node.next: {:?}", node.next);
-                println!("list_other: {:?}", list_other);
+                // println!("other's current value {} <= next value {}, swapping other and next", other_val, next_val);
+                // println!("node.next: {:?}", node.next);
+                // println!("list_other: {:?}", list_other);
                 let tmp = node.next.take();
-                println!("tmp: {:?}", tmp);
+                // println!("tmp: {:?}", tmp);
                 node.next = list_other.take();
                 list_other = tmp;
             }
-            println!("{}", node.val);
+            // println!("{}", node.val);
             current = node.next.as_mut();
         }
-        println!("{:?}", list_start);
+        // println!("{:?}", list_start);
         list_start
     }
 }
