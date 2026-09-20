ac28cf60fa9627f74118cb78a7db075d6c528752
knn217 <86541274+knn217@users.noreply.github.com>
2026-09-20 18:10:53 +0700

    Time: 0 ms (100%), Space: 2.1 MB (94.06%) - LeetHub

--- 0027-remove-element/0027-remove-element.rs
+++ 0027-remove-element/0027-remove-element.rs
@@ -8,18 +8,18 @@ impl Solution {
             // If no need to replace, continue
             if nums[idx] != val { continue; }
             // Found val, current index need replacing
-            println!("Found val in {}, need replacing", idx);
+            // println!("Found val in {}, need replacing", idx);
             val_counter += 1;
-            println!("Searching reverse in [{}, {}]", idx + 1, index_rev_mem);
+            // println!("Searching reverse in [{}, {}]", idx + 1, index_rev_mem);
             for idx_rev in ((idx + 1)..index_rev_mem).rev() {
                 // If value can't be use for replace, continue
                 if nums[idx_rev] == val {
-                    println!("Found val in {} when looking for replacement", idx_rev);
+                    // println!("Found val in {} when looking for replacement", idx_rev);
                     val_counter += 1;
                     continue;
                 }
                 // Found value to replace, perform replacement
-                println!("Replacing {}|{} with {}|{}", idx, nums[idx], idx_rev, nums[idx_rev]);
+                // println!("Replacing {}|{} with {}|{}", idx, nums[idx], idx_rev, nums[idx_rev]);
                 nums[idx] = nums[idx_rev];
                 // Mark this as the new point to look up replacements next time
                 index_rev_mem = idx_rev;
@@ -27,10 +27,10 @@ impl Solution {
                 continue 'outer;
             }
             // Replacement not found
-            println!("Replacement not found");
+            // println!("Replacement not found");
             break;
         }
-        println!("{}, {}", nums.len(), val_counter);
+        // println!("{}, {}", nums.len(), val_counter);
         (nums.len() - val_counter) as i32
     }
 }
