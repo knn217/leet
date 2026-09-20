4f8a0614df5a38712e8abdf211c0404d8d71e6ed
knn217 <86541274+knn217@users.noreply.github.com>
2026-09-20 18:25:12 +0700

    Time: 0 ms (100%), Space: 2.3 MB (8%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.rs
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.rs
@@ -1,11 +1,9 @@
 impl Solution {
     pub fn reverse_degree(s: String) -> i32 {
         let mut sum: i32 = 0;
-        let mut idx = 1;
-        for b in s.as_bytes(){
-            sum += ((123 - b) as i32) * idx;
-            // println!("{} | {}", 123 - b, sum);
-            idx += 1;
+        for (idx, byte) in s.as_bytes().iter().enumerate() {
+            sum += ((123 - byte) as i32) * ((idx + 1) as i32);
+            // println!("{} | {}", 123 - byte, sum);
         }
         sum
     }
