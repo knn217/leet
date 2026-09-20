fb58c5cf5c042028d62c4e359c68990f61143baa
knn217 <86541274+knn217@users.noreply.github.com>
2026-09-20 08:25:23 +0700

    Time: 0 ms (100%), Space: 2.2 MB (66.82%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.rs
+++ 0013-roman-to-integer/0013-roman-to-integer.rs
@@ -1,21 +1,24 @@
-use std::collections::HashMap;
-
 impl Solution {
+    fn map(ch: &char) -> i32 {
+        match *ch
+        {
+            'I' => {return 1;}
+            'V' => {return 5;}
+            'X' => {return 10;}
+            'L' => {return 50;}
+            'C' => {return 100;}
+            'D' => {return 500;}
+            'M' => {return 1000;}
+            _ => {return 0;}
+        }
+        return 0;
+    }
     pub fn roman_to_int(s: String) -> i32 {
-        let map: HashMap<char, i32> = HashMap::from([
-            ('I', 1),
-            ('V', 5),
-            ('X', 10),
-            ('L', 50),
-            ('C', 100),
-            ('D', 500),
-            ('M', 1000),
-        ]);
         let mut sum = 0;
         let mut curr_val = 0;
         for (idx, ch) in s.chars().rev().enumerate() {
             let prev_val = curr_val;
-            curr_val = *map.get(&ch).unwrap();
+            curr_val = Self::map(&ch);
             if curr_val >= prev_val {
                 sum += curr_val;
             } else {
