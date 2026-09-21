5d2bc7a987428a3e2cd5658cc1d0096b0fa79fa8
knn217 <86541274+knn217@users.noreply.github.com>
2026-09-21 23:34:42 +0700

    Time: 34 ms (14.29%), Space: 3.9 MB (28.57%) - LeetHub

--- 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.rs
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.rs
@@ -1,27 +1,29 @@
 impl Solution {
     pub fn result_array(nums: Vec<i32>, k: i32) -> Vec<i64> {
-        let k_usize = k as usize;
-        let mut total_counts = vec![0i64; k_usize];
-        // dp[r] stores the number of valid subarrays ending at the current position where the product modulo k equals r.
-        let mut dp = vec![0i64; k_usize];
+        let nums_len = nums.len() as i64;
+        let k = k as usize;
+        let mut res = vec![0i64; k];
+        res[0] = nums_len * (nums_len + 1) / 2;
+        // map[r] stores the number of valid subarrays ending at the current position where the product modulo k equals r.
+        let mut map = vec![0i64; k];
         for &num in &nums {
-            let mut next_dp = vec![0i64; k_usize];
-            let val = (num % k) as usize;
+            let mut next_map = vec![0i64; k];
+            let remainder = num as usize % k;
             // Option 1: Start a new contiguous subarray with just `num`
-            next_dp[val] += 1;
+            next_map[remainder] += 1;
             // Option 2: Extend all contiguous subarrays ending at the previous element
-            for prev_rem in 0..k_usize {
-                if dp[prev_rem] > 0 {
-                    let new_rem = (prev_rem * val) % k_usize;
-                    next_dp[new_rem] += dp[prev_rem];
-                }
+            for prev_remainder in 0..k {
+                if map[prev_remainder] <= 0 { continue; }
+                let new_remainder = (prev_remainder * remainder) % k;
+                next_map[new_remainder] += map[prev_remainder];
             }
-            // Accumulate into total counts and update dp state
-            for r in 0..k_usize {
-                total_counts[r] += next_dp[r];
+            // Accumulate into total counts and update map state
+            for r in 1..k {
+                res[0] -= next_map[r];
+                res[r] += next_map[r];
             }
-            dp = next_dp;
+            map = next_map;
         }
-        total_counts
+        res
     }
 }
