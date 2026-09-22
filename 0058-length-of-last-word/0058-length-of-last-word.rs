66797178d4846207e62e61a0cfb2b6cc4e9ee421
knn217 <86541274+knn217@users.noreply.github.com>
2026-09-23 00:34:01 +0700

    Time: 0 ms (100%), Space: 2.2 MB (63.04%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.rs
@@ -0,0 +1,10 @@
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+        let mut len = 0;
+        for ch in s.chars().rev() {
+            if ch != ' ' { len += 1; }
+            else if len != 0 { return len; }
+        }
+        len
+    }
+}
