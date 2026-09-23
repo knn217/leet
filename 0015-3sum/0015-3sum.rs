2ba4b1945915b6bdbdf339762ad38c2c82d4a985
knn217 <86541274+knn217@users.noreply.github.com>
2026-09-23 23:57:54 +0700

    Time: 16 ms (84.46%), Space: 3.9 MB (87.11%) - LeetHub

--- 0015-3sum/0015-3sum.rs
+++ 0015-3sum/0015-3sum.rs
@@ -1,33 +1,53 @@
 impl Solution {
-    pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut nums = nums;
-        nums.sort(); // O(N*log(N))
-        // println!("nums: {:?}", nums);
-        let mut res: Vec<Vec<i32>> = vec![];
-        for (idx_i, val_i) in nums.iter().enumerate() {
-            // Break if val_i > 0 since val_j, val_k > val_i
-            if *val_i > 0 { break; }
-            // Continue if val_i was repeated
-            if idx_i > 0 && nums[idx_i - 1] == *val_i { continue; }
-            let mut idx_j = idx_i + 1;
-            let mut idx_k = nums.len() - 1;
-            while idx_j < idx_k {
-                let val_j = nums[idx_j];
-                let val_k = nums[idx_k];
-                let sum = val_i + val_j + val_k;
+    pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let n = nums.len();
+        if n < 3 {
+            return vec![];
+        }
+
+        nums.sort_unstable(); // Slower than sort() for tiny slices, but skips stability guarantees
+
+        let mut res = Vec::new();
+
+        for i in 0..n - 2 {
+            let val_i = nums[i];
+
+            // Minimal sum of remaining elements is already > 0
+            if val_i > 0 {
+                break;
+            }
+
+            // Skip duplicates for first element
+            if i > 0 && nums[i - 1] == val_i {
+                continue;
+            }
+
+            let mut j = i + 1;
+            let mut k = n - 1;
+
+            while j < k {
+                let sum = val_i + nums[j] + nums[k];
+
                 if sum == 0 {
-                    res.push(Vec::from([*val_i, val_j, val_k]));
-                    // println!("idx_i: {}, idx_j: {}, idx_k: {}", idx_i, idx_j, idx_k);
-                    // println!("val_i: {}, val_j: {}, val_k: {}", val_i, val_j, val_k);
-                    // println!("res: {:?}", res);
-                    while idx_j < idx_k && val_j == nums[idx_j] { idx_j += 1; }
-                    while idx_j < idx_k && val_k == nums[idx_k] { idx_k -= 1; }
+                    res.push(vec![val_i, nums[j], nums[k]]);
+
+                    let val_j = nums[j];
+                    let val_k = nums[k];
+
+                    while j < k && nums[j] == val_j {
+                        j += 1;
+                    }
+                    while j < k && nums[k] == val_k {
+                        k -= 1;
+                    }
+                } else if sum < 0 {
+                    j += 1;
+                } else {
+                    k -= 1;
                 }
-                else if sum < 0 { idx_j += 1; }
-                else if sum > 0 { idx_k -= 1; }
-                if val_k <= 0 { break; }
             }
         }
+
         res
     }
 }
