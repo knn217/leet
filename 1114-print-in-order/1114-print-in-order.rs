941e066c21bc625cc0aed49df1364541be038e6e
knn217 <86541274+knn217@users.noreply.github.com>
2026-09-23 07:28:31 +0700

    Time: 0 ms (100%), Space: 2.5 MB (92.65%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.rs
+++ 1114-print-in-order/1114-print-in-order.rs
@@ -12,6 +12,7 @@ impl Foo {
     }
 
     pub fn first(&self, print_first: impl Fn()) {
+        // printFirst() outputs "first". Do not change or remove this line.
         print_first();
         *self.state.lock().unwrap() = 2;
         self.cv.notify_all();
@@ -20,8 +21,9 @@ impl Foo {
     pub fn second(&self, print_second: impl Fn()) {
         let mut step = self.state.lock().unwrap();
         // Automatically loops while the predicate returns true (*step < 2)
-        step = self.cv.wait_while(step, |s| *s < 2).unwrap();
+        step = self.cv.wait_while(step, |s| { return*s < 2; }).unwrap();
 
+        // printSecond() outputs "second". Do not change or remove this line.
         print_second();
 
         *step = 3;
@@ -30,8 +32,9 @@ impl Foo {
 
     pub fn third(&self, print_third: impl Fn()) {
         let mut step = self.state.lock().unwrap();
-        step = self.cv.wait_while(step, |s| *s < 3).unwrap();
+        step = self.cv.wait_while(step, |s| { return*s < 3; }).unwrap();
 
+        // printThird() outputs "third". Do not change or remove this line.
         print_third();
     }
 }
