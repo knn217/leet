865faea6ed35684899ae145be9c8b54496ecaee1
knn217 <86541274+knn217@users.noreply.github.com>
2026-09-26 22:45:41 +0700

    Time: 0 ms (100%), Space: 2.1 MB (91.67%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.rs
+++ 0069-sqrtx/0069-sqrtx.rs
@@ -1,23 +1,14 @@
 impl Solution {
     pub fn my_sqrt(x: i32) -> i32 {
-        if x == 0 || x == 1 { return x; }
-        let x = x as f64;
-        let mut high = x;
-        let mut low = 0_f64;
-        let mut target = (high + low) / 2_f64;
-        let mut square = f64::MAX;
-        loop {
-            square = target * target;
-            // println!("target: {:?}, square: {:?}, square-1: {:?}, x: {:?}", target, square, square-1.0, x);
-            if square == x { return target.floor() as i32; }
-            // let offset = target.max(1.0);
-            let offset = 1.0;
-            if x < square && (square - offset) < x { return target.floor() as i32; }
-            else if square < x { low = target; }
-            else if square > x { high = target; }
-            target = (high + low) / 2_f64;
+        if x == 0 { return 0; }
+        
+        let mut r = x as i64;
+        let target = x as i64;
+
+        while r * r > target {
+            r = (r + target / r) / 2;
         }
-        // println!("target: {:?}, square: {:?}, square-1: {:?}, x: {:?}", target, square, square-1.0, x);
-        target.floor() as i32
+
+        r as i32
     }
 }
