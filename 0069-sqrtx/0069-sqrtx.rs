ca20987a9fb18cc01bbe3dd2f210898cade000ce
knn217 <86541274+knn217@users.noreply.github.com>
2026-09-26 23:14:04 +0700

    Time: 0 ms (100%), Space: 2.1 MB (91.67%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.rs
+++ 0069-sqrtx/0069-sqrtx.rs
@@ -5,6 +5,9 @@ impl Solution {
         let mut r = x as i64;
         let target = x as i64;
 
+        // Newton method: r(n+1) = r(n) - f(r(n))/f'(r(n))
+        // f(r) = r^2 - x = 0
+        // => r(n+1) = (r(n) + (x / r(n))) / 2
         while r * r > target {
             r = (r + target / r) / 2;
         }
